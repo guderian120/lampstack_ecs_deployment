f71fc6b906bbe3829553d257d3d252efdb587c97
guderian120 <realamponsah10@yahoo.com>
2025-06-11 08:07:28 +0000

    reintegrated the compute module

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -3,12 +3,12 @@ output "instance_ids" {
   value       = aws_instance.this[*].id
 }
 
-output "instance_public_ips" {
-  description = "List of public IP addresses of EC2 instances"
-  value       = aws_eip.this[*].public_ip
-}
+# output "instance_public_ips" {
+#   description = "List of public IP addresses of EC2 instances"
+#   value       = aws_eip.this[*].public_ip
+# }
 
-output "instance_private_ips" {
-  description = "List of private IP addresses of EC2 instances"
-  value       = aws_instance.this[*].private_ip
-}
+# output "instance_private_ips" {
+#   description = "List of private IP addresses of EC2 instances"
+#   value       = aws_instance.this[*].private_ip
+# }
